a1471f9568496bfde40d08bb946d2cfd6d201ea9
Varun Kumar <wiservarun@gmail.com>
2020-12-16 22:17:02 +0530

    Intial Commit

---
+++ PackageFileReader/DTO/Datum.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageFileReader
+{
+    public class Datum
+    {
+        [JsonProperty("@id")]
+        public string Id { get; set; }
+        [JsonProperty("@type")]
+        public string Type { get; set; }
+        public string registration { get; set; }
+        public string id { get; set; }
+        public string version { get; set; }
+        public string description { get; set; }
+        public string summary { get; set; }
+        public string title { get; set; }
+        public string iconUrl { get; set; }
+        public string licenseUrl { get; set; }
+        public string projectUrl { get; set; }
+        public List<string> tags { get; set; }
+        public List<string> authors { get; set; }
+        public int totalDownloads { get; set; }
+        public bool verified { get; set; }
+        public List<PackageType> packageTypes { get; set; }
+        public List<Version> versions { get; set; }
+    }
+}
---
+++ PackageFileReader/Program.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PackageFileReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> nugDict = GetCurrentDictionary(ConfigurationManager.AppSettings["NugetFilePath"].ToString());
+            List<string> versionList = GetVersionList(nugDict, ConfigurationManager.AppSettings["SolutionPath"].ToString());
+            CreateCSV(versionList, ConfigurationManager.AppSettings["OutPutPath"].ToString());
+        }
+
+        private static List<string> GetVersionList(Dictionary<string, string> nugDict, string PackageFilePath)
+        {
+            string[] files = Directory.GetFiles(PackageFilePath, "packages.config", SearchOption.AllDirectories);
+
+            List<string> csv = new List<string>();
+            HashSet<string> set = new HashSet<string>();
+            foreach (string file in files)
+            {
+                var xml = XDocument.Load(file);
+
+                var elements = xml.Root.Descendants("package");
+
+                foreach (XElement element in elements)
+                {
+                    var id = element.Attribute(XName.Get("id", "")).Value;
+                    var version = element.Attribute(XName.Get("version", "")).Value;
+                    var targetFramework = element.Attribute(XName.Get("targetFramework", "")).Value;
+                    var latestVersion = string.Empty;
+                    if (nugDict.ContainsKey(id.ToLower()))
+                        latestVersion = nugDict[id.ToLower()];
+
+                    var patharr = file.Split('\\');
+                    var path = patharr[patharr.Length - 2];
+
+                    if (set.Contains(id) || id == null)
+                        continue;
+
+                    if (id != null && id.Length > 6 && id.Substring(0, 6).ToLower() == "system")
+                        continue;
+                    set.Add(id);
+                    csv.Add(id + "," + version + "," + targetFramework + "," + path + "," + latestVersion);
+                }
+            }
+            return csv;
+        }
+
+        private static void CreateCSV(List<string> csv, string CsvFilePath)
+        {
+            File.WriteAllLines(CsvFilePath, csv);
+        }
+
+        private static Dictionary<string, string> GetCurrentDictionary(string JsonFilePath)
+        {
+            string jsonFile = File.ReadAllText(JsonFilePath);
+            var json = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonFile);
+            Root root = Newtonsoft.Json.JsonConvert.DeserializeObject<Root>(jsonFile);
+            Dictionary<string, string> nugDict = new Dictionary<string, string>();
+
+            foreach (var data in root.data)
+            {
+                var keyarr = data.id.Split('/');
+                var key = keyarr[0].ToLower();
+                if (!nugDict.ContainsKey(key))
+                    nugDict.Add(key, data.version);
+            }
+
+            return nugDict;
+        }
+    }
+}
